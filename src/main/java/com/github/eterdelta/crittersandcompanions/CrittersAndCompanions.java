649030c9fa7d7ad98e11ba6152b58a416b84a130
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2023-08-02 17:15:44 -0400

    1.20.1 port

--- src/main/java/com/github/eterdelta/crittersandcompanions/CrittersAndCompanions.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/CrittersAndCompanions.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import software.bernie.geckolib3.GeckoLib;
+import software.bernie.geckolib.GeckoLib;
 
 @Mod(CrittersAndCompanions.MODID)
 public class CrittersAndCompanions {
